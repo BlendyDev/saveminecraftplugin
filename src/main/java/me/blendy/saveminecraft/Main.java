053b4fd08225c9794e4e83ddfd3e16aa2e7f5e2a
BuildTools <unconfigured@null.spigotmc.org>
2022-06-30 00:03:59 +0200

    Initial commit

---
+++ src/main/java/me/blendy/saveminecraft/Main.java
@@ -0,0 +1,98 @@
+package me.blendy.saveminecraft;
+
+import me.blendy.saveminecraft.files.ConfigFile;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.parser.Parser;
+
+import java.io.IOException;
+
+
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        plugin = this;
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                try {
+                    enableConfigs();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.runTaskLater(this, 1);
+    }
+    public void onDisable() {
+        try {
+            disableConfigs();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void enableConfigs() throws IOException {
+        ConfigFile.loadConfig();
+    }
+    public void disableConfigs() throws IOException {
+        ConfigFile.setConfig(changeOrgCooldown);
+    }
+    private long changeOrgLastTimestamp = -1;
+    public static int changeOrgCooldown = 30;
+    public static Main plugin;
+    public static Main getInstance() {
+        return plugin;
+    }
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        switch (command.getName().toLowerCase()) {
+            case "changeorg" -> {
+                long currentTimeMillis = System.currentTimeMillis();
+                if (changeOrgLastTimestamp == -1 || currentTimeMillis - changeOrgLastTimestamp > changeOrgCooldown*1000) {
+                    String url = "https://www.change.org/p/minecraft-s-1-19-s-new-chat-moderation-is-dangerous-broken-saveminecraft";
+                    changeOrgLastTimestamp = currentTimeMillis;
+                    try {
+                        int signatures = getSignatures(url);
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "A total of &l" + signatures + "&r people have signed!\nGo sign at &b&n" + url + "&r!"));
+
+                    } catch (IOException e) {
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cIOException"));
+                        e.printStackTrace();
+                    }
+                } else {
+                    sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&cThis command is on cooldown! Try again in &b" + (int) (changeOrgCooldown - Math.floor((currentTimeMillis-changeOrgLastTimestamp)/1000)) + "&c seconds!"));
+                }
+
+            }
+            case "savemc" -> {
+                if (args.length > 0 && args[0].equalsIgnoreCase("reload")) {
+                    try {
+                        enableConfigs();
+                        changeOrgLastTimestamp = -1;
+                        sender.sendMessage(ChatColor.translateAlternateColorCodes('&', "&aConfig reloaded!"));
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            }
+            default -> {
+            }
+        }
+        return true;
+    }
+
+    int getSignatures (String url) throws IOException {
+        Document doc = Jsoup.connect(url).
+                ignoreContentType(true).parser(Parser.xmlParser()).get();
+        String text = doc.select("script").first().text().substring(20);
+        JSONObject object = new JSONObject(text);
+        return (int) (((JSONObject) ((JSONObject) object.get("petition")).get("signatureCount")).get("total"));
+    }
+}
---
+++ src/main/java/me/blendy/saveminecraft/files/ConfigFile.java
@@ -0,0 +1,51 @@
+package me.blendy.saveminecraft.files;
+
+import me.blendy.saveminecraft.Main;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ConfigFile {
+    static File configFile;
+    static FileConfiguration config;
+    public static void createConfig () throws IOException {
+        Main plugin = Main.getInstance();
+        configFile = new File(plugin.getDataFolder(), "config.yml");
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdir();
+            configFile.createNewFile();
+            config = new YamlConfiguration();
+            setDefaults();
+            config.save(configFile);
+        }
+        config = new YamlConfiguration();
+        try {
+            config.load(configFile);
+        } catch (IOException | InvalidConfigurationException ex) {
+            ex.printStackTrace();
+        }
+    }
+    public static void setDefaults() {
+        config.set("cooldown", 30);
+    }
+    public static FileConfiguration getConfig() {
+        return config;
+    }
+
+    public static void setConfig (int cooldown) throws IOException {
+        createConfig();
+        config.set("cooldown", cooldown);
+        config.save(configFile);
+    }
+
+    public static void loadConfig() throws IOException {
+        createConfig();
+        try {
+            Main.changeOrgCooldown = (int) config.get("cooldown");
+        } catch (NullPointerException ex) {}
+    }
+
+}
