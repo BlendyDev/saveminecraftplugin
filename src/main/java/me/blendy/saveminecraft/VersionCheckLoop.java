66cee65bd2255b7273b0623cc2a17b0ca074f383
BlendyDev <blendydev@gmail.com>
2022-08-10 21:29:40 +0200

    Version 1.2 - Config and async update Added packet and sign GUI handlers

--- src/main/java/me/blendy/saveminecraft/VersionCheckLoop.java
+++ src/main/java/me/blendy/saveminecraft/VersionCheckLoop.java
@@ -3,15 +3,15 @@ package me.blendy.saveminecraft;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.parser.Parser;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class VersionCheckLoop extends BukkitRunnable {
-    public static int gitTags = 2;
+    public static int gitTags = 3;
     @Override
     public void run() {
         try {
@@ -21,7 +21,7 @@ public class VersionCheckLoop extends BukkitRunnable {
         }
     }
 
-    public static void reRun(Main plugin, long period) {
+    public static void reRun(SaveMinecraft plugin, long period) {
         plugin.versionCheckTask.cancel();
         plugin.versionCheckLoop.cancel();
         plugin.versionCheckLoop = new VersionCheckLoop();
@@ -29,10 +29,10 @@ public class VersionCheckLoop extends BukkitRunnable {
     }
     public static void checkForNewVersions() throws IOException {
         Document doc = Jsoup.connect("https://github.com/BlendyDev/saveminecraftplugin").ignoreContentType(true).parser(Parser.xmlParser()).get();
-        String text = doc.getElementsByClass("ml-3 Link--primary no-underline").first().text();
+        String text = Objects.requireNonNull(doc.getElementsByClass("ml-3 Link--primary no-underline").first()).text();
         int tags = Integer.parseInt(text.split(" ")[0]);
         if (tags > gitTags) {
-            String tag = doc.getElementsByClass("Link--primary d-flex no-underline").first().attributes().get("href");
+            String tag = Objects.requireNonNull(doc.getElementsByClass("Link--primary d-flex no-underline").first()).attributes().get("href");
             Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes('&', "&b[SaveMinecraft] &eA new version of the plugin is available! Download it at &nhttps://github.com" + tag));
         }
     }
--- src/main/java/me/blendy/saveminecraft/files/ConfigFile.java
+++ src/main/java/me/blendy/saveminecraft/files/ConfigFile.java
@@ -1,22 +1,23 @@
 package me.blendy.saveminecraft.files;
 
-import me.blendy.saveminecraft.Main;
+import me.blendy.saveminecraft.SaveMinecraft;
+import me.blendy.saveminecraft.commands.ChangeOrg;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConfigFile {
     static File configFile;
     static FileConfiguration config;
     public static void createConfig () throws IOException {
-        Main plugin = Main.getInstance();
+        SaveMinecraft plugin = SaveMinecraft.getInstance();
         configFile = new File(plugin.getDataFolder(), "config.yml");
         if (!configFile.exists()) {
-            configFile.getParentFile().mkdir();
-            configFile.createNewFile();
             config = new YamlConfiguration();
             setDefaults();
             config.save(configFile);
@@ -29,26 +30,39 @@ public class ConfigFile {
         }
     }
     public static void setDefaults() {
-        config.set("cooldown", 30);
-        config.set("versionCheckTime", 7200);
+        config.set("changeOrgCooldown", 30);
+        config.set("versionCheckTime", 1800);
+        List<String> administrators = Arrays.asList("BlendyGamer", "deathlife23", "mayuna_");
+        config.set("administrators", administrators);
     }
     public static FileConfiguration getConfig() {
         return config;
     }
+    public static File getConfigFile() {return configFile;}
 
-    public static void setConfig (int cooldown, int versionCheckTime) throws IOException {
+    public static void setConfig (int cooldown, int versionCheckTime, List<String> administrators) throws IOException {
         createConfig();
-        config.set("cooldown", cooldown);
+        config.set("changeOrgCooldown", cooldown);
         config.set("versionCheckTime", versionCheckTime);
+        config.set("administrators", administrators);
         config.save(configFile);
     }
 
+    @SuppressWarnings("unchecked")
     public static void loadConfig() throws IOException {
         createConfig();
         try {
-            Main.changeOrgCooldown = (int) config.get("cooldown");
-            Main.versionCheckTime = (int) config.get("versionCheckTime");
-        } catch (NullPointerException ex) {}
+            ChangeOrg.changeOrgCooldown = (int) config.get("changeOrgCooldown");
+            SaveMinecraft.versionCheckTime = (int) config.get("versionCheckTime");
+            SaveMinecraft.pluginAdministrators = (List<String>) config.get("administrators");
+        } catch (NullPointerException ex) {
+            config = new YamlConfiguration();
+            setDefaults();
+            config.save(configFile);
+            ChangeOrg.changeOrgCooldown = (int) config.get("changeOrgCooldown");
+            SaveMinecraft.versionCheckTime = (int) config.get("versionCheckTime");
+            SaveMinecraft.pluginAdministrators = (List<String>) config.get("administrators");
+        }
     }
 
 }
